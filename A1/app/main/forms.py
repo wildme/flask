027df13075a5a8c112d556dfe5f49247d5779b9a
Konstantin Ashaev <kostya@gorgaz.ru>
2020-10-15 15:55:47 +0300

    Added a form to edit user profile

--- A1/app/main/forms.py
+++ A1/app/main/forms.py
@@ -1,8 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, DateField, FileField
 from flask_wtf.file import FileRequired, FileAllowed
-from wtforms.validators import DataRequired, Length
-from ..models import Outbox
+from wtforms.validators import DataRequired, Length, Email, Regexp
+from ..models import Outbox, User
+
+class UserEdit(FlaskForm):
+    firstname = StringField('Firstname',
+            validators=[DataRequired(), Length(1, 64),
+                Regexp('^[A-Za-z]*$', 0, 'only letters allowed')])
+    lastname = StringField('Lastname',
+            validators=[DataRequired(), Length(1, 64),
+                Regexp('^[A-Za-z]*$', 0, 'only letters allowed')])
+    email = StringField('Email',
+            validators=[DataRequired(), Length(1, 64), Email()])
+    submit =  SubmitField('Save')
 
 class OutboxNew(FlaskForm):
     subject = StringField('Subject', validators=[DataRequired(), Length(1, 64)])
