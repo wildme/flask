ee0c504b9aa4109ff235ca5d56a68b3d25286df9
Konstantin Ashaev <konstantin.ashaev@gmail.com>
2020-10-18 22:13:13 +0300

    Fixed syntax mistakes and rerordered SQL statments

--- A1/mydb.sql
+++ A1/mydb.sql
@@ -1,20 +1,22 @@
+-- TABLE: users
 CREATE TABLE users
 (id TINYINT UNSIGNED AUTO_INCREMENT,
     login VARCHAR(20),
     password_h VARCHAR(128),
     firstname VARCHAR(30),
     lastname VARCHAR(30),
+    administrator TINYINT(1),
     email VARCHAR(64),
     dep_id TINYINT UNSIGNED,
-    CONSTRAINT pk_user PRIMARY KEY (id),
-    CONSTRAINT fk_deps_id FOREIGN KEY (dep_id)
-    REFERENCES departments (id)
-);
+    CONSTRAINT pk_user PRIMARY KEY (id)
+    );
+-- TABLE: departments
 CREATE TABLE departments
 (id TINYINT UNSIGNED AUTO_INCREMENT,
     name VARCHAR(30),
     CONSTRAINT pk_deps PRIMARY KEY (id)
 );
+-- TABLE: outbox
 CREATE TABLE outbox
 (id TINYINT UNSIGNED AUTO_INCREMENT,
     subject VARCHAR(256),
@@ -23,7 +25,16 @@ CREATE TABLE outbox
     user_id TINYINT UNSIGNED,
     notes VARCHAR(256),
     attachment VARCHAR(128),
-    CONSTARINT pk_outbox PRIMARY KEY (id),
-    CONSTRAINT fk_users_id FOREIGN KEY (user_id)
-    REFERENCES users (id)
+    CONSTRAINT pk_outbox PRIMARY KEY (id)
 );
+-- FOREIGN KEYS
+-- Reference: fk_deps_id table: users
+ALTER TABLE users ADD CONSTRAINT fk_deps_id
+    FOREIGN KEY (dep_id)
+    REFERENCES departments (id);
+-- Reference: fk_users_id table: outbox
+ALTER TABLE outbox ADD CONSTRAINT fk_users_id
+    FOREIGN KEY (user_id)
+    REFERENCES users (id);
+-- INSERT VALUES
+INSERT INTO users (login, firstname, lastname, administrator, email) VALUES ('admin', 'Admin', 'Admin', 1, "admin@example.com");  
