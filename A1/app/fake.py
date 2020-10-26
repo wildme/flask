82482b0a2310b1d13b9f49e292bc34d26fbaf6d2
Konstantin Ashaev <kostya@gorgaz.ru>
2020-10-26 16:08:17 +0300

    Content generator to test pagination

---
+++ A1/app/fake.py
@@ -0,0 +1,24 @@
+from random  import randint
+from sqlalchemy.exc import IntegrityError
+from faker import Faker
+from . import db
+from .models import Outbox
+from datetime import datetime
+
+def letters(count=50):
+    now = datetime.now()
+    dt_str = now.strftime('%Y-%m-%d %H:%M:%S')
+    fake = Faker()
+    i = 0
+    while i < count:
+        o = Outbox(subject=fake.text(),
+                user_id = 6,
+                reg_date = dt_str,
+                recipient=fake.name(),
+                notes=fake.text())
+        db.session.add(o)
+        try:
+            db.session.commit()
+            i += 1
+        except IntegrityError:
+            db.session.rollback()
