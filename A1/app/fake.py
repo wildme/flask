fd3aee82c992e7fd7fffe02b60ba51fbe8de4244
Konstantin Ashaev <kostya@gorgaz.ru>
2020-10-26 16:22:16 +0300

    modified fake.py

--- A1/app/fake.py
+++ A1/app/fake.py
@@ -2,17 +2,18 @@ from random  import randint
 from sqlalchemy.exc import IntegrityError
 from faker import Faker
 from . import db
-from .models import Outbox
+from .models import Outbox, User
 from datetime import datetime
 
 def letters(count=50):
     now = datetime.now()
     dt_str = now.strftime('%Y-%m-%d %H:%M:%S')
+    u = User.query.filter_by(login='kostya').first()
     fake = Faker()
     i = 0
     while i < count:
         o = Outbox(subject=fake.text(),
-                user_id = 6,
+                user_id = u.id,
                 reg_date = dt_str,
                 recipient=fake.name(),
                 notes=fake.text())
